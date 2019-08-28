0ea5c545b2cd5aa864320db62e9ed28ed9a08175
Michael-Choi <Michael.choi@queensu.ca>
2019-08-28 16:42:10 +0000

    added directional control for snake

--- input.js
+++ input.js
@@ -1,13 +1,24 @@
 const stdin = process.stdin;
+let connection;
+
+let directions = {
+  w: "Move: up",
+  s: "Move: down",
+  a: "Move: left",
+  d: "Move: right"
+};
 const handleUserInput = () => {
   stdin.on("data", key => {
     if (key === "\u0003") {
       process.exit();
+    } else {
+      connection.write(directions[key]);
     }
   });
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
