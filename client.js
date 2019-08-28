9e447ac99cedd7c8544f46e6e675efa202e5777e
Michael-Choi <Michael.choi@queensu.ca>
2019-08-28 14:15:27 +0000

    added snake connection to server

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("connection established");
+    conn.write("Name: MC");
+  });
+
+  // interpret incoming data as text
+  conn.on("data", data => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
