0d9d7938df7fa11f60e59eee3d7d9a1b8360813a
Michael-Choi <Michael.choi@queensu.ca>
2019-08-28 15:54:23 +0000

    test commit

--- play.js
+++ play.js
@@ -1,4 +1,12 @@
-const connect = require("./client");
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
